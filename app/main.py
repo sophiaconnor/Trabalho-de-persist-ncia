3742401f19c191cd566ffa22e5828107791b83d3
sophiaconnor <sophiamunizdeoliveira2006@gmail.com>
2026-08-30 13:57:47 -0300

    f14concluido

--- app/main.py
+++ app/main.py
@@ -2,19 +2,24 @@ from fastapi import FastAPI
 from app.services.backup_service import executar_backup
 from app.routes.backup import router as backup_router
 
+# Cria uma instância do FastAPI
 app = FastAPI()
+# Inclui o roteador de backup na aplicação FastAPI
 app.include_router(backup_router)
 
 @app.post("/backup")
 def fazer_backup():
-    
+
+    #executar o backup chamando a função executar_backup
     resultado = executar_backup()
 
+    #checa se o resultado é None, indicando que o backup já existe
     if resultado is None:
         return {
             "mensagem": "O backup já existe."
         }
 
+    #retorna uma mensagem de sucesso e o caminho do arquivo de backup criado
     return {
         "mensagem": "Backup realizado com sucesso.",
         "arquivo": resultado
--- app/routes/backup.py
+++ app/routes/backup.py
@@ -1,10 +1,24 @@
 from fastapi import APIRouter
+from app.services.backup_service import executar_backup
 
+# Definir o roteador para a rota de backup
 router = APIRouter()
 
 
 @router.post("/backup")
 def fazer_backup():
+
+    #executar o backup chamando a função executar_backup
+    resultado = executar_backup()
+
+    #checa se o resultado é None, indicando que o backup já existe
+    if resultado is None:
+        return {
+            "mensagem": "O backup já existe."
+        }
+
+    #retorna uma mensagem de sucesso e o caminho do arquivo de backup criado
     return {
-        "mensagem": "Rota de backup funcionando!"
+        "mensagem": "Backup realizado com sucesso.",
+        "arquivo": resultado
     }
